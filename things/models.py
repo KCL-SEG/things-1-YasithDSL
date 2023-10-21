5d07ca010171dd192f91dff6af8709c8630a6cd7
Yasith Medagama Disanayakage <yasithdsl@gmail.com>
2023-10-21 18:50:26 +0100

    task2

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    username = models.CharField(unique=True)
+    description = models.CharField()
+    quantity = models.IntegerField()
